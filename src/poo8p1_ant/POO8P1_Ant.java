f34608955f48f7a8009563a632844425f63c0eac
BereGzDl <146896863+BereGzDl@users.noreply.github.com>
2023-10-06 15:42:24 -0600

    Creación de la práctica

---
+++ src/poo8p1_ant/POO8P1_Ant.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poo8p1_ant;
+
+/**
+ *
+ * @author González De Luna Berenice
+ */
+public class POO8P1_Ant {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo desde POO grupo 8 con Ant");
+    }
+    
+}
